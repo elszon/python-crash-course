4c90f6d5ded68f89e5bc5c0514262edba7cc90ff
elszon <elszon@o2.pl>
2019-01-06 22:28:08 +0100

    init

---
+++ alien_invasion.py
@@ -0,0 +1,26 @@
+import pygame
+
+from settings import Settings
+from ship import Ship
+import game_functions as gf
+
+
+def run_game():
+    # Initialize pygame, setting, and create a screen object.
+    pygame.init()
+
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+
+    # Make a ship
+    ship = Ship(screen)
+
+    # Start the main loop for the game.
+    while True:
+        gf.check_events()
+        gf.update_screen(ai_settings, screen, ship)
+
+
+if __name__ == '__main__':
+    run_game()
---
+++ game_functions.py
@@ -0,0 +1,22 @@
+import sys
+
+import pygame
+from ship import Ship
+from settings import Settings
+
+
+def check_events():
+    """Respond to keypresses and mouse events."""
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+
+def update_screen(ai_settings: Settings, screen, ship: Ship):
+    """Update images on the screen and flip to the new screen."""
+    # Redraw the screen during each pass through the loop.
+    screen.fill(ai_settings.bg_color)
+    ship.blitme()
+
+    # Make the most recently drawn screen visible.
+    pygame.display.flip()
