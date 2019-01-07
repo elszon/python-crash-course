67e69adae7ce57ea2e8c7501b780c977de4b8f43
elszon <elszon@o2.pl>
2019-01-07 21:57:36 +0100

    movements

--- game_functions.py
+++ game_functions.py
@@ -5,11 +5,31 @@ from ship import Ship
 from settings import Settings
 
 
-def check_events():
+def check_events(ship: Ship):
     """Respond to keypresses and mouse events."""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            check_keydown_events(event, ship)
+        elif event.type == pygame.KEYUP:
+            check_keyup_events(event, ship)
+
+
+def check_keydown_events(event, ship):
+    """Respond to keypresses."""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+
+
+def check_keyup_events(event, ship):
+    """Respond to key releases."""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
 
 
 def update_screen(ai_settings: Settings, screen, ship: Ship):
